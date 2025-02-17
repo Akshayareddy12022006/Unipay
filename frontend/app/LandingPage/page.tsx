bf962cacad5864c0c7489ef936bb315cea722f6c
sudharshan <leelasudharshan2005@gmail.com>
2025-02-17 20:37:35 +0530

    Component Update

--- frontend/app/LandingPage/page.tsx
+++ frontend/app/LandingPage/page.tsx
@@ -1,18 +1,17 @@
-// import { NavbarDemo } from "@/components/Navbar"
-// import { AppleCardsCarouselDemo } from "@/components/Components"
 import { CarouselDemo } from "@/components/Carousel"
+import { FloatingDockDemo } from "@/components/Dock"
 
 export default function LandingPage() {
     return (
         <>
-            <div className="h-[100vh] bg-gradient-to-b from-white to-gray-900">
-                {/* <div className="p-">
-                <NavbarDemo />
-                </div>  */}
-                <div className="m-16 ml-0 mr-0">
+            <div className="h-[150vh] bg-gradient-to-b from-white to-gray-900">
+                <div className="m-0 ml-0 mr-0">
                     <CarouselDemo />
+                </div>
+                <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 mb-4">
+                    <FloatingDockDemo />
                 </div>
             </div>
         </>
-    )
+    );
 }
