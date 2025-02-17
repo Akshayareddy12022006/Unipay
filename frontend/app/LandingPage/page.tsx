8a31338754c00d5f3547e4a7c3af8d6dd282ab2a
sudharshan <leelasudharshan2005@gmail.com>
2025-02-17 19:35:55 +0530

    Add Components

---
+++ frontend/app/LandingPage/page.tsx
@@ -0,0 +1,11 @@
+import { NavbarDemo } from "@/components/Navbar"
+
+export function Home() {
+    return (
+        <>
+            <div className="h-[100vh] bg-gradient-to-b from-white to-gray-900">
+                <NavbarDemo />
+            </div>
+        </>
+    )
+}
