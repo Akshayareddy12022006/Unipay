73c470bbb41d7fa32cf1bdf7781e333a3acc06eb
sudharshan <leelasudharshan2005@gmail.com>
2025-02-19 21:16:54 +0530

    Responsive

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -2,7 +2,7 @@ import ImageCard from "./ui/cardItem";
 
 const Card = () => {
   return (
-        <div className="flex flex-row justify-between w-[70%]">
+        <div className="flex flex-row justify-between w-[70%] flex-wrap">
             <ImageCard
             title="Convert"
             imageSrc="https://images.unsplash.com/photo-1448375240586-882707db888b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3"
